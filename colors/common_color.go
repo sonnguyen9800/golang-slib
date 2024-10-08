779961da205d985499dcec1d92d941d16008239b
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2024-10-08 21:47:57 +0700

    color script

---
+++ colors/common_color.go
@@ -0,0 +1,25 @@
+package colors
+
+// ANSI escape codes for colored output
+const (
+	green = "\033[32m"
+	red   = "\033[31m"
+	reset = "\033[0m"
+	bold  = "\033[1m"
+)
+
+// Color functions for better usage
+func Green(text string) string {
+	return green + text + reset
+}
+
+func Red(text string) string {
+	return red + text + reset
+}
+
+func Bold(text string) string {
+	return bold + text + reset
+}
+func Reset(text string) string {
+	return reset + text
+}
